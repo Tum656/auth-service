fb79b7b97fad3f0a9e8cf8b751150023da6e7677
Tum656 <62370856+Tum656@users.noreply.github.com>
2025-08-16 14:38:48 +0700

    add auth and connect data base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'center',
+      password: '123456123',
+      database: 'auth',
+      autoLoadEntities: true,
+      synchronize: true, // Set to false in production!
+    }),AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

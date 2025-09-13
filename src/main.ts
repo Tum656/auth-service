fb0ea862812c7b9bd8cc1942bb3b7536b9c6e958
Tum656 <62370856+Tum656@users.noreply.github.com>
2025-09-13 16:02:18 +0700

    Edit app.use set session

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,26 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 import { TransformInterceptor } from './common/filters/transform.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
-    app.use(
-    session({
-      secret: 'my-secret-key',
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 3600000 },
-    }),
-  );
+
+app.use(
+  session({
+    secret: configService.get<string>('SESSION_SECRET')!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60, // 1 ชั่วโมง
+      secure: process.env.NODE_ENV === 'production', // ควรเปิดเมื่อใช้ https
+    },
+  }),
+);
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
